ab3ed2f6a426e467e145a3d1ab575cfc182e0803
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:12:21 +0530

    Add files via upload

---
+++ ifClasse.java
@@ -0,0 +1,14 @@
+package conditnalstatements;
+
+public class ifClasse {
+
+	public static void main(String[] args) {
+		int age = 19;
+		if(age>=18) {
+			System.out.println("you can vote now");
+			
+		}
+		
+	}
+
+}
