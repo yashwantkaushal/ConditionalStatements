c4db313ad75fa2212ea79d154f0191a7fc59bb83
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:12:49 +0530

    Add files via upload

---
+++ ifElseClass.java
@@ -0,0 +1,18 @@
+package conditnalstatements;
+
+public class ifElseClass {
+
+	public static void main(String[] args) {
+		
+		int noOfPetals = 12;
+		
+		if(noOfPetals % 2 == 0) {
+			System.out.println("He loves me not");
+			
+		}else {
+			System.out.println("He love me");
+		}
+
+	}
+
+}
