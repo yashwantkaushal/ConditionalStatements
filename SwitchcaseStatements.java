beb360c24c751733b84c8430268bfea2391506e9
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:13:58 +0530

    Add files via upload

---
+++ SwitchcaseStatements.java
@@ -0,0 +1,73 @@
+package conditnalstatements;
+
+public class SwitchcaseStatements {
+
+	public static void main(String[] args) {
+		
+		int dayOfWeek = 2;
+		
+//		if(dayOfWeek == 1){
+//			
+//		}
+//		
+//		if(dayOfWeek == 2) {
+//			
+//		}
+//	    if(dayOfWeek == 3){
+//			
+//		}
+//		
+//		if(dayOfWeek == 4) {
+//			
+//		}
+//			
+//	    if(dayOfWeek == 5){
+//			
+//		}
+//		
+//		if(dayOfWeek == 6) {
+//			
+//		}
+//		
+//		if(dayOfWeek == 7) {
+//		
+//		}
+		
+//		switch(dayOfWeek) {
+//		case 1:
+//			System.out.println("I am on leav");
+//			break;
+//		case 2:
+//			System.out.println("I am in office");
+//			break;
+//		case 3:
+//			System.out.println("i am playing football");
+//			break;
+//			
+//			default:
+//				System.out.println("I don't know what day it is"); 
+//		}
+		
+		 int rating = 5;
+		 
+		 switch(rating) {
+		 case 1:
+		 case 2:
+			 
+			 System.out.println("Bad review");
+			 break;
+			 
+		  case 3:
+			  
+			 System.out.println("Average review");
+			 break;
+		  case 4:
+		  case 5:
+			 System.out.println("Good review");
+			 break;
+			
+			 
+		 }
+	}
+
+}
